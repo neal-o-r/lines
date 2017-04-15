fa41197c4741fed4ab0dc3d6a55cdacae867f85f
neal-o-r <neal@n-o-r.xyz>
2017-04-15 22:25:47 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,7 @@
+# Lines
+---
+
+Line fitting in Julia. Least Squares fitting for now.
+
+![LSTSQ](https://github.com/neal-o-r/lines/raw/master/lstsq.png)
+
---
+++ line.jl
@@ -0,0 +1,82 @@
+using PyPlot
+using Distributions
+
+
+srand(123)
+
+function get_data(m::Float64, b::Float64, N::Int64)
+
+	x = sort(10 * rand(N))
+	u = 2 * rand(N)
+	y = m * x + b + u .* rand(N)
+
+	return x, y, u
+
+end
+
+function least_sq(x::Array{Float64, 1}, y::Array{Float64, 1}, 
+			u::Array{Float64,1})
+
+	A = hcat(ones(length(x)), x)
+
+	# Error matrix
+	C = diagm(u .* u)
+	# bias and slope
+	b, m = inv(A' * A) * A' * y
+	
+	# Cov matrix
+	covar = inv(A' * \(C, A))
+	b_un, m_un = 1.96 * sqrt(covar[1, 1]), 1.96 * sqrt(covar[2, 2])
+
+	return m, b, m_un, b_un	
+
+end
+
+
+function conf_region(x::Array{Float64, 1}, y::Array{Float64, 1}, 
+			m::Float64, b::Float64)
+
+	n = length(x)
+	t_crit = quantile(TDist(n - 2), 1 - 0.95/2) 	
+	s_y = sqrt(sum((y - x * m  + b).^2) / (n - 2))
+	x_bar = mean(x)
+	s_x = sqrt(sum((x - mean(x)).^2) / n)
+
+	ci = t_crit * s_y * sqrt(1 / n + (x - x_bar).^2 / ((n - 1) * s_x))
+	
+	return ci
+
+end
+
+
+function make_chart(x::Array{Float64, 1}, y::Array{Float64, 1}, 
+		u::Array{Float64, 1}, m::Float64, 
+		b::Float64, ci::Array{Float64, 1})
+		
+	y_ls = x * m + b
+
+	plot(x, y_ls, "k--")
+	fill_between(x, y_ls - ci, y_ls + ci, alpha=0.8, color="0.75")
+	errorbar(x, y, yerr=u, fmt=".k")
+
+end
+
+
+function main()
+
+	m_true = 1.128
+	b_true = 4.159
+	x, y, u = get_data(m_true, b_true, 20)
+
+	m_ls, b_ls, m_un_ls, b_un_ls = least_sq(x, y, u)
+
+	ci = conf_region(x, y, m_ls, b_ls)
+
+	make_chart(x, y, u, m_ls, b_ls, ci)
+
+	@printf("Least Squares
+	 	m = %.3f (m_true = %.3f)
+		b = %.3f (b_true = %.3f)", m_ls, m_true, b_ls, b_true)
+
+
+end
